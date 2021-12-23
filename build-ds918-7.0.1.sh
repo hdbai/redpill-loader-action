25f9b9628a0a14291a8cb5b1bbddee8d5086aa93
HDB <TBC3211@GMAIL.COM>
2021-12-23 13:06:09 +0800

    Update build-ds918-7.0.1.sh

--- build-ds918-7.0.1.sh
+++ build-ds918-7.0.1.sh
@@ -34,7 +34,8 @@ cp ${root}/user_config.DS918+.json ./user_config.json
 ./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/igb/rpext-index.json
 ./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/ixgbe/rpext-index.json
 ##https://xpenology.com/forum/profile/58145-thorgroup/content/?type=forums_topic_post&change_section=1
-./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/mpt3sas/rpext-index.json
+##mpt3sas驱动最新版有问题 https://github.com/pocopico/4.4.180plus-modules/issues/1 改为10月19日版本
+./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/56ec2571ddd5167fc1e9d34ce508924dbe8f8f32/mpt3sas/rpext-index.json
 ./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/r8125/rpext-index.json
 ./ext-manager.sh add https://raw.githubusercontent.com/pocopico/rp-ext/main/rtl8150/rpext-index.json
 ##virtio和mpt2sas兼容问题导致无限循环重装
